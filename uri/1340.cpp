83ad0102a4e198ac56a15ea38a52cb2d4363eca0
Lorena Raphaela Diniz Leao <lorenalleao@gmail.com>
2020-03-10 18:56:48 -0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA WOOHOO! IT WORKS!

--- uri/1340.cpp
+++ uri/1340.cpp
@@ -1,78 +1,80 @@
 #include <bits/stdc++.h>
 
-using namespace std ;
+using namespace std;
+
+template <typename T>
+void clear(T &x){
+	while(x.size()) x.pop();
+}
 
 int main(){
-	int n;
-	
+	int n; 
+
+	priority_queue<int> pq;
+	queue<int> q;
+	stack<int> s;
+
 	while(cin >> n){
-		
-		list<int> l;
-		
-		int op, number, max, queue_counter, stack_counter, pqueue_counter, removals;
-		bool impossible = true;
-		priority_queue<int> pqueue;
-		
-		max = -1;
-		queue_counter = 0; stack_counter = 0; pqueue_counter = 0;
-		removals = 0;
-		
+
+		bool stack, queue, pqueue, impossible;
+
+		stack = true;
+		queue = true;
+		pqueue = true;
+		impossible = false;
+
 		for(int i = 0; i < n; i++){
-			
+			int op, number;
 			cin >> op >> number;
 
-			impossible = true;
-			
 			switch(op){
 				case 1:
-					l.push_back(number);
-					pqueue.push(number);
+					q.push(number);
+					s.push(number);
+					pq.push(number);
 					break;
-				
 				case 2:
-					removals++;
-					cout << number <<endl;
-					cout << pqueue.top() <<endl;
-					cout << l.front() <<endl;
-					cout << l.back() <<endl;
-					if(pqueue.top() == number){
-						pqueue_counter++;
-						pqueue.pop();
-						impossible = false;
+					if(stack){
+						if(s.size() and s.top() == number){
+							s.pop();
+						} else
+							stack = false;
 					}
-					if(l.front() == number){
-						queue_counter++;
-						l.pop_front();
-						impossible = false;
+
+					if(queue){
+						if(q.size() and q.front() == number){
+							q.pop();
+						} else
+							queue = false;
 					}
-					if(l.back() == number){ //problema aqui nessa lista compartilhada pra fazer pop no if de cima e nesse
-						stack_counter++;
-						l.pop_back();
-						impossible = false;
+
+					if(pqueue){
+						if(pq.size() and pq.top() == number){
+							pq.pop();
+						} else
+							pqueue = false;
 					}
-			}
-			
-			if(impossible and op == 2){
-				cout << "impossible\n";
-				break;
 			}
 		}
-		cout << removals << endl;
-		cout << pqueue_counter << endl;
-		cout << queue_counter << endl;
-		cout << stack_counter << endl;
-		if(pqueue_counter == removals){
-			if((queue_counter == removals) != (stack_counter == removals))
-				cout << "not sure\n";
-			else if(queue_counter != removals and stack_counter != removals)
-				cout << "priority queue\n";
-			else
-				cout << "impossible\n";
-		} else if(queue_counter == removals)
-			cout << "queue\n";
-		else
-			cout << "stack\n";
 
+		if(!stack and !queue and !pqueue){
+			impossible = true;
+		}
+
+		if(impossible)
+			cout << "impossible\n";
+		else if((stack and pqueue) or (queue and pqueue))
+			cout << "not sure\n";
+		else if(stack)
+			cout << "stack\n";
+		else if(queue)
+			cout << "queue\n";
+		else if(pqueue)
+			cout << "priority queue\n";
+
+		clear(q);
+		clear(pq);
+		clear(s);
 	}
 
 	return 0;
