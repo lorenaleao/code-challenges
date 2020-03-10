d30440a1ed1ff52ff7ce4198c2a75562a0f00cfe
lorenaleao <lorenalleao@gmail.com>
2020-03-10 01:04:29 -0300

    continued to implement the solution to p. #1340. not done

--- uri/1340.cpp
+++ uri/1340.cpp
@@ -7,41 +7,72 @@ int main(){
 	
 	while(cin >> n){
 		
-		vector<int> v;
+		list<int> l;
 		
-		int op, number, max;
-		bool queue, stack, pqueue;
+		int op, number, max, queue_counter, stack_counter, pqueue_counter, removals;
+		bool impossible = true;
+		priority_queue<int> pqueue;
 		
 		max = -1;
-		queue = false; stack = false; pqueue = false;
+		queue_counter = 0; stack_counter = 0; pqueue_counter = 0;
+		removals = 0;
 		
 		for(int i = 0; i < n; i++){
 			
 			cin >> op >> number;
+
+			impossible = true;
 			
 			switch(op){
 				case 1:
-					if(number > max)
-						max = number;
-					v.push_back(number);
+					l.push_back(number);
+					pqueue.push(number);
 					break;
 				
 				case 2:
-					if(v[0] == number)
-						queue = true;
-					if(v[v.size()-1] == number)
-						stack = true;
-					if(number == max)
-					//problema: n vou saber o segundo maior dese jeito, 
-						//saber o max a cada intervalo? 
-						//fazer vetor de booleanos e setar quando for adicionado?
-
-
-
+					removals++;
+					cout << number <<endl;
+					cout << pqueue.top() <<endl;
+					cout << l.front() <<endl;
+					cout << l.back() <<endl;
+					if(pqueue.top() == number){
+						pqueue_counter++;
+						pqueue.pop();
+						impossible = false;
+					}
+					if(l.front() == number){
+						queue_counter++;
+						l.pop_front();
+						impossible = false;
+					}
+					if(l.back() == number){ //problema aqui nessa lista compartilhada pra fazer pop no if de cima e nesse
+						stack_counter++;
+						l.pop_back();
+						impossible = false;
+					}
 			}
 			
-
+			if(impossible and op == 2){
+				cout << "impossible\n";
+				break;
+			}
 		}
+		cout << removals << endl;
+		cout << pqueue_counter << endl;
+		cout << queue_counter << endl;
+		cout << stack_counter << endl;
+		if(pqueue_counter == removals){
+			if((queue_counter == removals) != (stack_counter == removals))
+				cout << "not sure\n";
+			else if(queue_counter != removals and stack_counter != removals)
+				cout << "priority queue\n";
+			else
+				cout << "impossible\n";
+		} else if(queue_counter == removals)
+			cout << "queue\n";
+		else
+			cout << "stack\n";
+
 	}
 
 	return 0;
