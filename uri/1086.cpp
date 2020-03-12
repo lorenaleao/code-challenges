d0b4df1324d33ec2af88d8ef464225c37cf24732
lorenaleao <lorenalleao@gmail.com>
2020-03-11 23:14:26 -0300

    started to solve p. #1086

---
+++ uri/1086.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int c; cin >> c;
+	for(int i = 0; i < c; i++){
+		int n; 
+		string infix, prefix;
+
+		cin >> n >> infix >> prefix;
+	}
+	return 0;
+}
