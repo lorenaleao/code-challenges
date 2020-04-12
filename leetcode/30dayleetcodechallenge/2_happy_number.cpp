7857eef9e2d7766f125a0e26e527c50f75a80ac8
lorenaleao <lorenalleao@gmail.com>
2020-04-11 22:49:34 -0300

    adding solution to day 2 problem

---
+++ leetcode/30dayleetcodechallenge/2_happy_number.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int sum_squares_digits(int n){
+        int sum = 0;
+        while(n){
+            int digit = n % 10;
+            n /= 10;
+            sum += digit * digit;
+        }
+        return sum;
+    }
+    
+    bool isHappy(int n) {
+        set<int> numbers;
+        
+        n = sum_squares_digits(n);
+        
+        while(numbers.count(n) == 0){
+            numbers.insert(n);
+            n = sum_squares_digits(n);
+        }
+        
+        if(n == 1)
+            return true;
+        else
+            return false;
+    }
+};
